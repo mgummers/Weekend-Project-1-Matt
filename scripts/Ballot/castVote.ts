4fcdc09aa883fac42d29362794b558dbeae05bbe
Matt Gummersbach <matt.gummersbach@gmail.com>
2022-07-10 10:18:56 -0500

    simplified

--- scripts/Ballot/castVote.ts
+++ scripts/Ballot/castVote.ts
@@ -8,7 +8,6 @@ const EXPOSED_KEY =
   "8da4ef21b864d2cc526dbdb2a120bd2874c36c9d0a1fb7f8c63d7f7a8b41de8f";
 
 async function main() {
-  // copy of wallet setup from deploy.ts
   const wallet = new ethers.Wallet(process.env.PRIVATE_KEY || EXPOSED_KEY);
   console.log(`Using address ${wallet.address}`);
 
@@ -28,7 +27,6 @@ async function main() {
   ) as Ballot;
 
   console.log("Voting....");
-  //   vote for proposal 1
   await ballotContract.vote(2);
 }
 
--- scripts/Ballot/deploy.ts
+++ scripts/Ballot/deploy.ts
@@ -6,7 +6,6 @@ import * as ballotJSON from "../../artifacts/contracts/Ballot.sol/Ballot.json";
 const EXPOSED_KEY =
   "8da4ef21b864d2cc526dbdb2a120bd2874c36c9d0a1fb7f8c63d7f7a8b41de8f";
 
-// helper function to deploy contract with string inputs from user in the CLI
 function convertStringArrayToBytes32(array: string[]) {
   const bytes32Array = [];
   for (let index = 0; index < array.length; index++) {
@@ -16,28 +15,24 @@ function convertStringArrayToBytes32(array: string[]) {
 }
 
 async function main() {
-  // we can attach the wallet to our private key, knowing we have it in the dotenv.
-  // the exposed key is added to keep typescript happy (always returns a value)
+
   const wallet = new ethers.Wallet(process.env.PRIVATE_KEY || EXPOSED_KEY);
   console.log(`Using address ${wallet.address}`);
 
   const provider = ethers.providers.getDefaultProvider("ropsten");
   const signer = wallet.connect(provider);
 
-  //.getBalance() returns of type BigNumber, we must parse this correctly to integrate w our scripts
   const balanceBigNumber = await signer.getBalance();
   const balance = Number(ethers.utils.formatEther(balanceBigNumber));
 
   console.log(`Wallet balance at ${wallet.address} is ${balance}`);
 
-  // confirm wallet has enough ether to operate
   if (balance < 0.01) {
     throw new Error("The wallet provided does not have enough ether!");
   }
 
   console.log("Deploying Ballot Contract");
 
-  // get proposals from node CLI arguments
   console.log("Proposals: ");
   const proposals = process.argv.slice(2);
 
@@ -45,19 +40,16 @@ async function main() {
     throw new Error("not enough proposals provided");
   }
 
-  // display proposals
   proposals.forEach((proposal, index) => {
     console.log(`Proposal No. ${index + 1}: ${proposal}`);
   });
 
-  // create contract factory with ethers, passing compiled json info as args
   const ballotFactory = new ethers.ContractFactory(
     ballotJSON.abi,
     ballotJSON.bytecode,
     signer
   );
 
-  // use convertStringArray helper to deploy with args as Bytes32
   const ballotContract = await ballotFactory.deploy(
     convertStringArrayToBytes32(proposals)
   );
